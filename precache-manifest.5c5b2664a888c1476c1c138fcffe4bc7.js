d4f7933111a913f77d4e1a1f29e90cc2d66ed73a
Fabrizio DUHEM <fabrizio@eriser.fr>
2020-07-27 22:12:58 +0200

    Updates

---
+++ precache-manifest.5c5b2664a888c1476c1c138fcffe4bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3724421cdda42ccfb49037e89786c91a",
+    "url": "/707/index.html"
+  },
+  {
+    "revision": "6877734e5d4dd454b00b",
+    "url": "/707/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "537a569c5559f4b85dec",
+    "url": "/707/static/js/2.a0d36cc7.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/707/static/js/2.a0d36cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6877734e5d4dd454b00b",
+    "url": "/707/static/js/main.e53c0584.chunk.js"
+  },
+  {
+    "revision": "af38d00b7c342888b6d0",
+    "url": "/707/static/js/runtime-main.0b054073.js"
+  }
+]);
